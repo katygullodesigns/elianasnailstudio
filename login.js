2865a5f5a45302c7cbaafe49c5b957e6affe4335
katygullodesigns <katygullodesigns@gmail.com>
2026-06-11 16:46:15 -0500

    login.js

--- login.js
+++ login.js
@@ -11,16 +11,3 @@ window.loginOwner = async function () {
     message.textContent = error.message;
   }
 };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCKIRvUsp8Ysv5iqzNUkySwZP2lEmB66JY",
-  authDomain: "elianasnailstudio.firebaseapp.com",
-  projectId: "elianasnailstudio",
-  storageBucket: "elianasnailstudio.firebasestorage.app",
-  messagingSenderId: "414202013471",
-  appId: "1:414202013471:web:78cdf850b270cf6d1fb22a"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
