782465d45c293fe70aeccd7726514a46d15477cc
katygullodesigns <katygullodesigns@gmail.com>
2026-06-11 16:03:05 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -9,16 +9,18 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKIRvUsp8Ysv5iqzNUkySwZP2lEmB66JY",
+  apiKey: "YOUR_API_KEY",
   authDomain: "elianasnailstudio.firebaseapp.com",
   projectId: "elianasnailstudio",
   storageBucket: "elianasnailstudio.firebasestorage.app",
   messagingSenderId: "414202013471",
-  appId: "1:414202013471:web:78cdf850b270cf6d1fb22a",
-  measurementId: "G-JP9J414SLM"
+  appId: "1:414202013471:web:78cdf850b270cf6d1fb22a"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
