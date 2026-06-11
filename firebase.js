1ca7e0b9ecd7080790103176a180283b379ca7d9
katygullodesigns <katygullodesigns@gmail.com>
2026-06-11 17:48:08 -0500

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,13 @@
 const firebaseConfig = {
-  apiKey: "...",
+  apiKey: "AIzaSyCKIRvUsp8Ysv5iqzNUkySwZP2lEmB66JY",
   authDomain: "elianasnailstudio.firebaseapp.com",
   projectId: "elianasnailstudio",
   storageBucket: "elianasnailstudio.firebasestorage.app",
   messagingSenderId: "414202013471",
-  appId: "1:414202013471:web:78cdf850b270cf6d1fb22a"
+  appId: "1:414202013471:web:78cdf850b270cf6d1fb22a",
+  measurementId: "G-JP9J414SLM"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
