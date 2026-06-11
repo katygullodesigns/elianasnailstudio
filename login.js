76046dd2a284bf42c3aef0429f556bc0a327303e
katygullodesigns <katygullodesigns@gmail.com>
2026-06-11 16:50:34 -0500

    login.js

--- login.js
+++ login.js
@@ -1,13 +1,11 @@
-document.getElementById("loginBtn").addEventListener("click", async function () {
+window.loginOwner = async function () {
   const email = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value;
-  const message = document.getElementById("loginMessage");
 
   try {
     await auth.signInWithEmailAndPassword(email, password);
     window.location.href = "appointments.html";
   } catch (error) {
     console.error(error);
-    message.textContent = error.message;
   }
-});
+};
