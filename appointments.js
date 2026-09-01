c6125f6a20e72f5e86240c4e913ad079359ae46a
katygullodesigns <katygullodesigns@gmail.com>
2026-08-31 23:25:42 -0500

    Update appointments.js

--- appointments.js
+++ appointments.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
   "https://kyonstvpolakjhrecqcj.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "PASTE_YOUR_EXACT_SUPABASE_ANON_KEY_HERE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdW9uc3R2cG9sYWtqaHJlY3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2OTYxMjUsImV4cCI6MjA5NzI3MjEyNX0.oq6v7gEy8FJPh4NI3ngUYybwJcHF6rW6qkNtepCxr7Y";;
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
