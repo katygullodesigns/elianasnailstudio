9d4245770ac04acf32fc6c9438e4e8ad5dc9c032
katygullodesigns <katygullodesigns@gmail.com>
2026-09-11 10:18:12 -0500

    Update appointments.js

--- appointments.js
+++ appointments.js
@@ -8,7 +8,6 @@ const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
-
 // ==========================================
 // OWNER
 // ==========================================
